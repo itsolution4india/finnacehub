a0f3a85cae07a6340bd3ffb3998ae7d5cd541ff3
dilipitsolutions4india-cloud <dilip.itsolutions4india@gmail.com>
2025-10-30 23:25:08 +0530

    Updated Core Functionality

--- core/urls.py
+++ core/urls.py
@@ -12,3 +12,10 @@ urlpatterns = [
 
     path('', views.index, name='index'),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
